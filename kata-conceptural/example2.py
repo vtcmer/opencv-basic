b7836d27d24254c9f5f127ad3098eface5d57137
Victor <vtcmer@gmail.com>
2020-11-22 18:06:56 +0100

    Faces detection

--- kata-conceptural/example2.py
+++ kata-conceptural/example2.py
@@ -98,26 +98,61 @@ def showColorDetection():
 #showColorDetection()
 
 
-def getContours(img):
+def getContours(img,imgContour):
     contours, hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        print(area)
+       
+        if area > 500:
+            #print(area)
+            cv2.drawContours(imgContour,cnt,-1,(255,0,0),3)
+            peri = cv2.arcLength(cnt,True)
+            #print(peri)
+            aprox = cv2.approxPolyDP(cnt,0.02*peri,True)
+            #print(len(aprox))
+            objColor = len(aprox)
+            x, y, w, h = cv2.boundingRect(aprox)
+
+            objDescription = "NaN"
+            if objColor == 3:
+                objDescription = 'Tri'
+            elif  objColor == 4:
+                aspRatio = w/float(h)
+                if (aspRatio > 0.95 and aspRatio < 1.05):
+                    objDescription = "Cua"
+                else:
+                    objDescription = "Rec"
+            elif  objColor == 5:
+                objDescription = 'Pen'
+            elif  objColor == 6:
+                objDescription = 'Hex'
+            else:
+                aspRatio = w/float(h)
+                print(aspRatio)
+                if (aspRatio == 1):
+                    objDescription = "Octo"
+                else:
+                    objDescription = "Cir"
+
+            cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(imgContour,objDescription,(x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,0),2)
 
 def showShapes():
     img = cv2.imread("resources/shapes.jpeg")
 
+    imgContour = img.copy()
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(img,(7,7),1)
     imgCanny = cv2.Canny(imgBlur,50,50)
     
-    getContours(imgCanny)
+    getContours(imgCanny,imgContour)
 
     cv2.imshow("Output",img)
     cv2.imshow("Output Gray",imgGray)
     cv2.imshow("Output Blur",imgBlur)
     cv2.imshow("Output Canny",imgCanny)
+    cv2.imshow("Output Contours",imgContour)
     cv2.waitKey(0)
 
-showShapes()
+#showShapes()
---
+++ kata-conceptural/faceDetection.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+#resize Image
+def showFace():
+
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_alt2.xml')
+
+    img = cv2.imread("resources/stark.jpg")
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(imgGray,1.1,4)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+   
+    cv2.imshow("Output",img)
+    cv2.waitKey(0)
+
+showFace()
